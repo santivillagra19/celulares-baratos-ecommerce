94aef45efd805cc8b7582fe2fb08cbb6b583c50c
Miniyloli.456 <santivillagra.45@gmail.com>
2026-06-08 13:50:22 -0300

    fix: arreglo en build de Chunk size limit

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'tiptap-vendor': ['@tiptap/react', '@tiptap/starter-kit', '@tiptap/pm'],
+          'supabase-vendor': ['@supabase/supabase-js']
+        }
+      }
+    }
+  }
 })
